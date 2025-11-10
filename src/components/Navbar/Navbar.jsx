38e8d62f561a123d6a87211a6c131386759aae0a
Emmanuel <emmanueljoeemie@gmail.com>
2025-11-10 22:00:00 +0100

    added new code to fix the toggle error

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,73 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+// import { CgProfile } from "react-icons/cg";
+// // import { HiMenuAlt4, HiX } from "react-icons/hi"; // Commented out since sidebar is disabled
+// // import { HiMenuAlt4 } from "react-icons/hi"; // Also commented out
+// // import { motion } from "framer-motion"; // Sidebar animation temporarily disabled
+// import "./Navbar.scss";
+
+// const Navbar = () => {
+//   // Sidebar toggle state (currently unused)
+//   const [toggle, setToggle] = useState(false);
+
+//   return (
+//     <nav className="app__navbar">
+//       {/* ===== Logo Section ===== */}
+//       <div className="app__navbar-logo">
+//         <a href="#home">
+//           <CgProfile className="logo_icon" size={40} />
+//           <p>EA</p>
+//         </a>
+//       </div>
+
+//       {/* ===== Main Navigation Links (Visible on larger screens) ===== */}
+//       <ul className="app__navbar-links">
+//         {["home", "about", "work", "contact", "skills"].map((item) => (
+//           <li className="app__flex p-text" key={`link-${item}`}>
+//             <a href={`#${item}`} onClick={() => setToggle(false)}>
+//               {item}
+//             </a>
+//           </li>
+//         ))}
+//       </ul>
+
+//       {/* ===== Sidebar Menu (Commented Out for Now) ===== */}
+//       {/*
+//       <div className="app__navbar-menu">
+//         <HiMenuAlt4 onClick={() => setToggle(true)} />
+//         {toggle && (
+//           <motion.div
+//             whileInView={{ x: [300, 0] }}
+//             transition={{ duration: 0.85, ease: "easeOut" }}
+//           >
+//             <ul>
+//               {["home", "about", "work", "contact", "skills"].map((item) => (
+//                 <li key={`${item}`}>
+//                   <a onClick={() => setToggle(false)} href={`#${item}`}>
+//                     {item}
+//                   </a>
+//                 </li>
+//               ))}
+//             </ul>
+//           </motion.div>
+//         )}
+//       </div>
+//       */}
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
+
+
+
+
+import React from "react";
 import { CgProfile } from "react-icons/cg";
-// import { HiMenuAlt4, HiX } from "react-icons/hi"; // Commented out since sidebar is disabled
-// import { HiMenuAlt4 } from "react-icons/hi"; // Also commented out
-// import { motion } from "framer-motion"; // Sidebar animation temporarily disabled
+// import { HiMenuAlt4, HiX } from "react-icons/hi";
+// import { motion } from "framer-motion";
 import "./Navbar.scss";
 
 const Navbar = () => {
-  // Sidebar toggle state (currently unused)
-  const [toggle, setToggle] = useState(false);
-
   return (
     <nav className="app__navbar">
       {/* ===== Logo Section ===== */}
@@ -23,9 +82,7 @@ const Navbar = () => {
       <ul className="app__navbar-links">
         {["home", "about", "work", "contact", "skills"].map((item) => (
           <li className="app__flex p-text" key={`link-${item}`}>
-            <a href={`#${item}`} onClick={() => setToggle(false)}>
-              {item}
-            </a>
+            <a href={`#${item}`}>{item}</a>
           </li>
         ))}
       </ul>
@@ -57,3 +114,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
