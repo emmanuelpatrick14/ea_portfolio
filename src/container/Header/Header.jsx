0fe4c53f254aad0445a4c86b8bfb8b329cc0a5e1
Emmanuel <emmanueljoeemie@gmail.com>
2025-11-10 04:13:45 +0100

    latest chnages

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,35 +1,35 @@
+
+
 import React, { useState, useEffect, useMemo } from "react";
 import { motion } from "framer-motion";
-import images from "../../constants/images";
+import images from "../../constants/images"; // overlay
 import "./Header.scss";
 import { AppWrap } from "../../wrapper";
 
 const Header = () => {
-  // List of titles, memoized to avoid recreating array on every render
   const texts = useMemo(
-    () => ["Emmanual Patrick", "A Full Stack Enginner", "Web Developr"],
+    () => ["Emmanuel Patrick", "A Full Stack Engineer", "Web Developer"],
     []
   );
 
   const [currentTextIndex, setCurrentTextIndex] = useState(0);
   const [currentText, setCurrentText] = useState(texts[0]);
 
-  // Update currentTextIndex every 4 seconds
   useEffect(() => {
     const intervalId = setInterval(() => {
       setCurrentTextIndex((prevIndex) => (prevIndex + 1) % texts.length);
     }, 4000);
 
-    return () => clearInterval(intervalId); // clean up interval on unmount
+    return () => clearInterval(intervalId);
   }, [texts.length]);
 
-  // Update currentText whenever currentTextIndex changes
   useEffect(() => {
     setCurrentText(texts[currentTextIndex]);
   }, [currentTextIndex, texts]);
 
   return (
     <div className="app__header app__flex">
+      {/* Info Section */}
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 0.5 }}
@@ -37,7 +37,7 @@ const Header = () => {
       >
         <div className="app__header-intro">
           <div className="intro-cmp app__flex">
-            <p className="p-text">Welcome to my portflio web site</p>
+            <p className="p-text">Welcome to my portfolio website</p>
             <div>
               <h1 className="head-text">
                 Hi, I'm <span style={{ color: "#314bac" }}>{currentText}</span>
@@ -47,30 +47,32 @@ const Header = () => {
 
           <div className="tag-cmp app__flex">
             <p className="app__head-desc">
-              Im a dynamic Full Stack Developer with a year of experiance in the tech
-              industry. I enjoy crafting web applictions that offer smooth and
-              engaging user experiences. Proficient in frontend frameworks like
-              React and Next.js, as well as backend using Express.js and NestJs.
-              I have a good understnding of web accessibility standards and
-              history of delivering projects on time and within budget. 
-              Always looking to learn more and contrbute to a collaborative team.
+              I'm a dynamic Full Stack Developer with two years of experience in
+              the tech industry. I enjoy crafting web applications that offer smooth
+              and engaging user experiences. Proficient in frontend frameworks like
+              React and Next.js, as well as backend using Express.js and NestJs. I
+              have a good understanding of web accessibility standards and a history
+              of delivering projects on time and within budget. Always looking to
+              learn more and contribute to a collaborative team.
             </p>
           </div>
         </div>
       </motion.div>
 
+      {/* Image Section */}
       <motion.div
         whileInView={{ opacity: [0, 1] }}
         transition={{ duration: 0.5, delayChildren: 0.5 }}
         className="app__header-img"
       >
-        <img src={images.profile} alt="profile" />
-        <motion.img
+        {/* Full portrait profile image */}
+        <img src="/prof.jpeg" alt="profile" className="profile-img" />
+
+        {/* Aesthetic overlay circle */}
+        <motion.div
           whileInView={{ scale: [0, 1] }}
           transition={{ duration: 1, ease: "easeInOut" }}
-          src={images.circle}
-          alt="overlay circle"
-          className="overlay_circle"
+          className="overlay-circle"
         />
       </motion.div>
     </div>
